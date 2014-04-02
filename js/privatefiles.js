603771c11114da8242135207b1783d296fb383e3
Alex Boyce <curiosity26@gmail.com>
2014-04-02 15:03:26 -0400

    Getting the text from the wrong element

--- js/privatefiles.js
+++ js/privatefiles.js
@@ -2,18 +2,19 @@
  * Created by alexboyce on 4/1/14.
  */
 (function($) {
-    Drupal.behaviors.privatefiles = {
-        attach: function(context, settings) {
-            $(".pf-hash-clipboard").click(function(event) {
-                event.preventDefault();
-                var url = location.protocol + '//' + location.hostname + '/private/file/' + $(this).text();
-                if (!!window.clipboardData) {
-                    window.clipboardData.setData("Text", url);
-                }
-                else {
-                    window.prompt("Press Ctrl + C to copy the path to the clipboard", url);
-                }
-            });
+  Drupal.behaviors.privatefiles = {
+    attach: function(context, settings) {
+      $(".pf-hash-clipboard").click(function(event) {
+        event.preventDefault();
+        var text = $(this).parent().parent().child("a").first().text();
+        var url = location.protocol + '//' + location.hostname + '/private/file/' + text;
+        if (!!window.clipboardData) {
+          window.clipboardData.setData("Text", url);
         }
-    };
+        else {
+          window.prompt("Press Ctrl + C to copy the path to the clipboard", url);
+        }
+      });
+    }
+  };
 })(jQuery);
